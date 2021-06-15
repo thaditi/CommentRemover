34fbf1313088112f6f3becb551959d7a8b13c8bf
Aditi Thapliyal <47235301+thaditi@users.noreply.github.com>
2021-06-15 15:24:51 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String [] args)
+    {
+        String fileRead="InputFile.txt";
+        String fileWrite="OutputFile.txt";
+
+        try {
+            FileReader fileReader = new FileReader(fileRead);
+            BufferedReader br = new BufferedReader(fileReader);
+
+            FileWriter fileWriter = new FileWriter(fileWrite);
+            BufferedWriter bw = new BufferedWriter(fileWriter);
+
+            String line=br.readLine();
+            while (line!=null)
+            {
+                if(line.contains("//")) {
+                    String word[] = line.split("//");
+                    if (word[0].trim().length() > 0) {
+                        bw.write(word[0]);
+                        bw.newLine();
+                    }
+                }
+                else if(line.contains("/*"))
+                {
+                    String word[] = line.split("/*");
+                    if (word[0].trim().length() > 0) {
+                        bw.write(word[0]);
+                        bw.newLine();
+                    }
+                    while (!line.contains("*/"))
+                        line=br.readLine();
+                    String temp[]=line.split("\\*/");
+                    if(temp.length>1)
+                        if(word[0].trim().length() > 0) {
+                            bw.write(word[0]);
+                            bw.newLine();
+                        }
+                }
+                else if (line.trim().length() > 0) {
+                    bw.write(line);
+                    bw.newLine();
+                }
+                line=br.readLine();
+            }
+            br.close();
+            bw.close();
+        }
+        catch (FileNotFoundException exception)
+        {
+            System.out.println("ERROR : File Not Found or File Name Incorrect");
+        }
+        catch (IOException exception)
+        {
+            System.out.println("ERROR : Unable to Write in File");
+        }
+    }
+}
